780d14517453bdb31a34d5e2df2ca822df68541b
Hollie Eggleton <hollie.eggleton987@gmail.com>
2020-12-07 18:04:36 -0500

    NAV

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,6 +3,7 @@ let navBarToggle = document.getElementById('js-navbar-toggle');
 
 navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
+  navBarToggle.classList.toggle('active');
 });
 
 
